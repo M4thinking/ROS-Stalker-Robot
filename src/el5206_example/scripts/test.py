f0ae87e29ebfe0941ef2de2408d789124b175b88
DiegoSanzB <diegosanz01@gmail.com>
2022-12-05 20:14:12 -0300

    Stalker detecta Famous y rota hacia el

--- src/el5206_example/scripts/test.py
+++ src/el5206_example/scripts/test.py
@@ -20,6 +20,7 @@ import tf2_ros
 import time
 import yaml
 
+from PIL import Image
 from geometry_msgs.msg import Twist, Pose2D
 from nav_msgs.msg import Odometry
 from sensor_msgs.msg import LaserScan, Image
@@ -53,6 +54,8 @@ class EL5206_Robot:
         self.target_yaw = None
         self.path = rospkg.RosPack().get_path('el5206_example')
 
+        self.camera_fov = 1.089
+
         # Extra variable to print odometry
         self.odom_i = 0
 
@@ -61,10 +64,11 @@ class EL5206_Robot:
         rospy.Subscriber("/ground_truth/state", Odometry,  self.groundTruthCallback)
         rospy.Subscriber("/scan",               LaserScan, self.scanCallback)
         rospy.Subscriber("/target_pose",        Pose2D,    self.poseCallback)
-        rospy.Subscriber("/stalker/camera_link_camera/camera_link_camera/depth/image_raw",  Image,     self.imageCallback)
+        rospy.Subscriber("/stalker/camera_link_camera/camera_link_camera/color/image_raw",  Image,     self.imageCallback)
+        rospy.Subscriber("/stalker/camera_link_camera/camera_link_camera/depth/image_raw",  Image,     self.depthCallback)
 
         # Publishers
-        self.vel_pub = rospy.Publisher('/cmd_vel', Twist, queue_size=1)
+        self.vel_pub = rospy.Publisher('/stalker/cmd_vel', Twist, queue_size=1)
 
         # Timer
         self.update_timer = rospy.Timer( rospy.Duration(1.0), self.timerCallback )
@@ -82,11 +86,15 @@ class EL5206_Robot:
     """
 
     def imageCallback(self, msg):
-
         image_np = np.frombuffer(msg.data, dtype=np.uint8).reshape(msg.height, msg.width, -1)
-        self.currentImage = image_np[:,:,:3]
-        self.currentDepth = image_np[:,:,3]
+        self.currentImage = image_np #[:,:,:3]
+        #self.currentDepth = image_np[:,:,3]
         #self.currentImage = self.bridge.imgmsg_to_cv(msg, "brg8")
+
+
+    def depthCallback(self, msg):
+        image_np = np.frombuffer(msg.data, dtype=np.uint8).reshape(msg.height, msg.width, -1)
+        self.currentDepth = image_np#[:,:,3]
 
 
     def scanCallback(self, msg):
@@ -261,22 +269,103 @@ class EL5206_Robot:
         
         return array_to_save
     
-    
+    def detect_famous(self, img):
+        lower = np.array([100, 0, 0])
+        upper = np.array([255, 0, 0])
+
+        mask = cv2.inRange(img, lower, upper)
+        
+        mask_bin = mask
+        mask_bin[mask_bin > 122] = 1
+
+        column_histogram = mask_bin.sum(axis=0)
+        row_histogram = mask_bin.sum(axis=1)
+
+        column_sum = column_histogram.sum()  
+        row_sum = row_histogram.sum()
+
+        x_famous = 0
+        y_famous = 0
+
+        for x in range(column_histogram.shape[0]):
+            x_famous += x*column_histogram[x]
+
+        for y in range(row_histogram.shape[0]):
+            y_famous += y*row_histogram[y]
+        
+        x_famous = int(x_famous/column_sum) if column_sum > 0 else 0
+        y_famous = int(y_famous/row_sum) if row_sum > 0 else 0
+
+        mask_bin[y_famous, x_famous] = 10
+
+        x_center = img.shape[1]/2
+
+        rotate_angle = (x_famous - x_center) * self.camera_fov / img.shape[1]
+
+        print(rotate_angle)
+
+        return cv2.circle(mask_bin, (x_famous, y_famous), 10, (10, 0, 0)), rotate_angle
+
+    def rotate_to(self, angle):
+        
+        vel_angular = -0.2
+        twist_msg = Twist()
+        twist_msg.linear.x  = 0.0
+        twist_msg.angular.z = vel_angular * np.sign(angle)
+        self.vel_pub.publish(twist_msg)
+        time.sleep(np.abs(angle/vel_angular))
+
+        twist_msg = Twist()
+        twist_msg.linear.x  = 0.0
+        twist_msg.angular.z = 0.0
+        self.vel_pub.publish(twist_msg)
+
+    def angle_follow(self, iterations=60):
+        for _ in range(iterations):
+            image = self.currentImage
+
+            detection, rotate_angle = self.detect_famous(image)
+            self.rotate_to(rotate_angle)
+            time.sleep(0.5)
+
+
+    def show_video(self, frames=60):
+        for _ in range(frames):
+            from matplotlib.colors import hsv_to_rgb
+
+
+            image = self.currentImage
+            image_rgb = hsv_to_rgb(image)
+            #print(image_rgb.shape)
+            #print(image)
+
+            #cv2.imshow('a', image)
+            detection, rotate_angle = self.detect_famous(image)
+            print(rotate_angle)
+            
+
+            plt.imshow(detection)
+            #self.rotate_to(rotate_angle)
+            #plt.draw()
+            plt.colorbar()
+            plt.pause(1)
+            plt.close()
+
 
     def cam_test(self, counts = 1):
         for _ in range(counts):
-            data = self.currentImage
+            image = self.currentImage
             #print(self.currentImage)
-            print(type(self.currentImage),"\n")
-            image_np = np.frombuffer(data.data, dtype=np.uint8).reshape(data.height, data.width, -1)
-            print(image_np.shape)
+            #print(type(self.currentImage),"\n")
+            print(image.shape)
             
-            depth = np.copy(image_np[:,:,3])
-#            depth[depth>100] = 0
-            print(depth.min(), depth.max())
+            #depth = np.copy(image_np[:,:,3])
+
+            depth = self.currentDepth
+            #depth[depth>100] = 0
+            print(depth.shape, depth.min(), depth.max())
             
-            
-            plt.imshow(depth/(127-62))
+            plt.imshow(depth[:,:,2])
             plt.colorbar()
             plt.show()
             time.sleep(1)
@@ -287,7 +376,9 @@ if __name__ == '__main__':
     time.sleep(2)
 
     try:
-        node.cam_test()
+        #node.angle_follow()
+        node.show_video(10)
+        #node.cam_test()
 
     except rospy.ROSInterruptException:
         rospy.logerr("ROS Interrupt Exception! Just ignore the exception!")
